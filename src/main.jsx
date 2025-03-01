01b45985ead954a1a9dd5dbf41955d76589270b3
elia-victor <serbasepulu@gmail.com>
2025-03-01 12:37:27 +0700

    Fix Github Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // 👈 Import BrowserRouter
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter basename="/excel-to-gmap"> {/* 👈 Set basename for GitHub Pages */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
